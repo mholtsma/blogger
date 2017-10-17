8f3c81e8c4d481bd7143a16368853188efe29bf2
marc holtsman <mholtsma@ucsc.edu>
2017-10-16 19:30:49 -0700

    Set up Express and Mongo

--- test-app/server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Get API listing
-router.get('/', function(req, res){
-  res.send('API works')
-});
-
-module.exports = router;
